550b4bc24d0f8c7554b9d44e42a6fa8210eda4f4
Caroline Larry <carolinelarry@Carolines-MBP.attlocal.net>
2021-05-22 15:10:23 -0500

    Adding final changes

--- app.py
+++ app.py
@@ -31,8 +31,8 @@ def welcome():
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/%3Cstart%3E<br/>"
-        f"/api/v1.0/<start>/<end>"
+        f"/api/v1.0/start<br/>"
+        f"/api/v1.0/start/end"
     )
 
 #Precipitation route
